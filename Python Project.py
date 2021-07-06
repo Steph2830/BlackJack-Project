34165cda78e9b88adb9dcfa8bc1e5ca3f531acf4
Steph2830 <84094385+Steph2830@users.noreply.github.com>
2021-07-06 20:01:24 -0230

    Update Python Project.py

--- Python Project.py
+++ Python Project.py
@@ -1,122 +1,130 @@
-# Function to print the cards
-def print_cards(cards, hidden):
-		
-	s = ""
-	for card in cards:
-		s = s + "\t ________________"
-	if hidden:
-		s += "\t ________________"
-	print(s)
+import random
+import os
+import time
+
+# Card class Definition
+class card:
+    def __init__(self, suit, value, cardValve):
+
+        #Suit of the Card(Spades,Clubs,Hearts,Diamonds)
+        self.suit = suit
+
+        #Value of the Card
+        self.value = value
+
+        #Score Value of the Card
+        self.cardValue = cardValue
+
+#Function to clear the Terminal
+    def clear():
+        os.system("clear")
+
+#Function for Printing the Cards
+    def print_cards(cards, hidden):
+
+        s = ""
+        for card in cards:
+            s = s + "\t ________________"
+        if hidden:
+            s += "\t ________________"
+        print(s)
 
 
-	s = ""
-	for card in cards:
-		s = s + "\t|                |"
-	if hidden:
-		s += "\t|                |"		
-	print(s)
+        s = ""
+        for card in cards:
+            s = s + "\t|                |"
+        if hidden:
+            s += "\t|                |"		
+        print(s)
 
-	s = ""
-	for card in cards:
-		if card.value == '10':
-			s = s + "\t|  {}            |".format(card.value)
-		else:
-			s = s + "\t|  {}             |".format(card.value)	
-	if hidden:
-		s += "\t|                |"		
-	print(s)
+        s = ""
+        for card in cards:
+            if card.value == '10':
+                s = s + "\t|  {}            |".format(card.value)
+            else:
+                s = s + "\t|  {}             |".format(card.value)	
+        if hidden:
+            s += "\t|                |"		
+        print(s)
 
-	s = ""
-	for card in cards:
-		s = s + "\t|                |"
-	if hidden:
-		s += "\t|      * *       |"	
-	print(s)	
+        s = ""
+        for card in cards:
+            s = s + "\t|                |"
+        if hidden:
+            s += "\t|      * *       |"	
+        print(s)	
 
-	s = ""
-	for card in cards:
-		s = s + "\t|                |"
-	if hidden:
-		s += "\t|    *     *     |"	
-	print(s)	
+        s = ""
+        for card in cards:
+            s = s + "\t|                |"
+        if hidden:
+            s += "\t|    *     *     |"	
+        print(s)	
 
-	s = ""
-	for card in cards:
-		s = s + "\t|                |"
-	if hidden:
-		s += "\t|   *       *    |"	
-	print(s)	
+        s = ""
+        for card in cards:
+            s = s + "\t|                |"
+        if hidden:
+            s += "\t|   *       *    |"	
+        print(s)	
 
-	s = ""
-	for card in cards:
-		s = s + "\t|                |"
-	if hidden:
-		s += "\t|   *       *    |"	
-	print(s)	
+        s = ""
+        for card in cards:
+            s = s + "\t|                |"
+        if hidden:
+            s += "\t|   *       *    |"	
+        print(s)	
 
-	s = ""
-	for card in cards:
-		s = s + "\t|       {}        |".format(card.suit)
-	if hidden:
-		s += "\t|          *     |"	
-	print(s)	
+        s = ""
+        for card in cards:
+            s = s + "\t|       {}        |".format(card.suit)
+        if hidden:
+            s += "\t|          *     |"	
+        print(s)	
 
-	s = ""
-	for card in cards:
-		s = s + "\t|                |"
-	if hidden:
-		s += "\t|         *      |"	
-	print(s)	
+        s = ""
+        for card in cards:
+            s = s + "\t|                |"
+        if hidden:
+            s += "\t|         *      |"	
+        print(s)	
 
-	s = ""
-	for card in cards:
-		s = s + "\t|                |"
-	if hidden:
-		s += "\t|        *       |"	
-	print(s)
+        s = ""
+        for card in cards:
+            s = s + "\t|                |"
+        if hidden:
+            s += "\t|        *       |"	
+        print(s)
 
-	s = ""
-	for card in cards:
-		s = s + "\t|                |"
-	if hidden:
-		s += "\t|                |"	
-	print(s)
+        s = ""
+        for card in cards:
+            s = s + "\t|                |"
+        if hidden:
+            s += "\t|                |"	
+        print(s)
 
-	s = ""
-	for card in cards:
-		s = s + "\t|                |"
-	if hidden:
-		s += "\t|                |"	
-	print(s)	
+        s = ""
+        for card in cards:
+            s = s + "\t|                |"
+        if hidden:
+            s += "\t|                |"	
+        print(s)	
 
-	s = ""
-	for card in cards:
-		if card.value == '10':
-			s = s + "\t|            {}  |".format(card.value)
-		else:
-			s = s + "\t|            {}   |".format(card.value)
-	if hidden:
-		s += "\t|        *       |"			
-	print(s)	
-		
-	s = ""
-	for card in cards:
-		s = s + "\t|________________|"
-	if hidden:
-		s += "\t|________________|"	
-	print(s)		
+        s = ""
+        for card in cards:
+            if card.value == '10':
+                s = s + "\t|            {}  |".format(card.value)
+            else:
+                s = s + "\t|            {}   |".format(card.value)
+        if hidden:
+            s += "\t|        *       |"			
+        print(s)	
+            
+        s = ""
+        for card in cards:
+            s = s + "\t|________________|"
+        if hidden:
+            s += "\t|________________|"	
+        print(s)		
 
-	print()
-	
-	# The Card Class definition
-class Card:
-	def __init__(self, suit, value, card_value):
-		
-		# Suit of the Card like Spades and Clubs
-		self.suit = suit
-
-		# Representing Value of the Card like A for Ace, K for King
-		self.value = value
-
-		# Score Value for the Card like 10 for King
-		self.card_value = card_value
+        print()                       
